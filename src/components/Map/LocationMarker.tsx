c254f1ac7110b9cd7e1abd86d52ce32c7a212604
“KazemiMohammad” <kazemi_mcp@hotmail.com>
2022-08-03 19:01:15 +0430

    fix (Map): Define custome icon for the marker

--- src/components/Map/LocationMarker.tsx
+++ src/components/Map/LocationMarker.tsx
@@ -1,6 +1,13 @@
+import L from "leaflet";
 import React from "react";
 import { Marker, Popup } from "react-leaflet";
-import { ILounch } from "../../data/lounch/types";
+import { ILounch } from "@/data/lounch/types";
+import icon from '../../assets/icons/marker.png';
+
+const customIcon = new L.Icon({
+  iconUrl: icon,
+  iconSize:[48,48]
+});
 
 interface ILocationMarkerProps {
   launches: ILounch[];
@@ -10,11 +17,12 @@ function LocationMarker({ launches }: ILocationMarkerProps) {
   if (launches.length) {
     return (
       <>
-        {launches.map((item: ILounch,index:number) => {
+        {launches.map((item: ILounch, index: number) => {
           return (
             <Marker
-              key={item.id+index}
+              key={item.id + index}
               position={[item.pad.latitude, item.pad.longitude]}
+              icon={customIcon}
             >
               <Popup>{item.name}</Popup>
             </Marker>
@@ -28,3 +36,4 @@ function LocationMarker({ launches }: ILocationMarkerProps) {
 }
 
 export default LocationMarker;
+
